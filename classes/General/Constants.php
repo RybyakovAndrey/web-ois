7a736615fc65972c5090517179c782bc2b9d95c9
cuug <rybyakovandrey8@gmail.com>
2025-11-29 02:25:58 +0500

    initialization lms

---
+++ classes/General/Constants.php
@@ -0,0 +1,71 @@
+<?php
+namespace Legacy\General;
+
+class Constants
+{
+	/** НОВОСТИ */
+	public const IB_FURNITURE_NEWS_S1 = '1';
+
+	/** [S1] ПРОДУКЦИЯ */
+	public const IB_FURNITURE_PRODUCTS_S1 = '2';
+
+	/** [S1] УСЛУГИ */
+	public const IB_FURNITURE_SERVICES_S1 = '3';
+
+	/** ВАКАНСИИ */
+	public const IB_FURNITURE_VACANCIES_S1 = '4';
+
+	/** АКЦИИ */
+	public const IB_PROMOTIONS = '5';
+
+	/** СПЕЦИАЛЬНОЕ ПРЕДЛОЖЕНИЕ */
+	public const IB_PROP_FURNITURE_PRODUCTS_S1_SPECIALOFFER = '1';
+
+	/** ЦЕНА */
+	public const IB_PROP_FURNITURE_PRODUCTS_S1_PRICE = '2';
+
+	/** ВАЛЮТА ЦЕНЫ (ПОДПИСЬ) */
+	public const IB_PROP_FURNITURE_PRODUCTS_S1_PRICECURRENCY = '3';
+
+	/** ГАБАРИТЫ (Д Х Ш Х В) */
+	public const IB_PROP_FURNITURE_PRODUCTS_S1_SIZE = '4';
+
+	/** РАЗМЕРЫ СПАЛЬНОГО МЕСТА */
+	public const IB_PROP_FURNITURE_PRODUCTS_S1_S_SIZE = '5';
+
+	/** АРТИКУЛ */
+	public const IB_PROP_FURNITURE_PRODUCTS_S1_ARTNUMBER = '6';
+
+	/** МАТЕРИАЛ */
+	public const IB_PROP_FURNITURE_PRODUCTS_S1_MATERIAL = '7';
+
+	/** ПРОИЗВОДИТЕЛЬ */
+	public const IB_PROP_FURNITURE_PRODUCTS_S1_MANUFACTURER = '8';
+
+	/** НАЗВАНИЕТИТЛ */
+	public const IB_PROP_PROMOTIONS_TITLE = '9';
+
+	/** ИЗОБРАЖЕНИЕ */
+	public const IB_PROP_PROMOTIONS_IMAGE = '10';
+
+	/** ПРЕВЬЮ */
+	public const IB_PROP_PROMOTIONS_PREVIEW_DESCRIPTION = '11';
+
+	/** БАДЖ */
+	public const IB_PROP_PROMOTIONS_BADGE = '12';
+
+	/** ДЕТАЛЬНЫЙ КОНТЕНТ */
+	public const IB_PROP_PROMOTIONS_DETAIL_CONTENT = '13';
+
+	/** ПОЛЬЗОВАТЕЛИ, ИМЕЮЩИЕ ПРАВО ГОЛОСОВАТЬ ЗА РЕЙТИНГ */
+	public const GROUP_RATING_VOTE = '3';
+
+	/** ПОЛЬЗОВАТЕЛИ ИМЕЮЩИЕ ПРАВО ГОЛОСОВАТЬ ЗА АВТОРИТЕТ */
+	public const GROUP_RATING_VOTE_AUTHORITY = '4';
+
+	/** КОНТЕНТ-РЕДАКТОРЫ */
+	public const GROUP_CONTENT_EDITOR = '5';
+
+	/** PROMOTIONS */
+	public const HLBLOCK_PROMOTIONS = '1';
+}
